1588cbab48639232a7802e8fccb539c5aeeeeda2
Olkobarylko <olegbarylko81@gmail.com>
2021-05-31 21:31:07 +0300

    StructureCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DiscoverComponent } from './pages/discover/discover.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { CreateComponent } from './pages/create/create.component';
+import { GoodsComponent } from './pages/goods/goods.component';
+import { ServiceComponent } from './pages/service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DiscoverComponent,
+    BlogComponent,
+    CreateComponent,
+    GoodsComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
